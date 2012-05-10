d065be46d4377dab34f0dae4058c51c431748393
fzzbt <juhanipm@gmail.com>
2012-05-10 16:11:45 +0300

    koodauiststa

---
+++ wadharkka/forms.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from django.forms import ModelForm, TextInput, Textarea
+from models import Document 
+
+class DocumentForm(ModelForm):
+    class Meta:
+        model = Document
+        fields = ('subject', 'content')
+        widgets = {
+            'subject': TextInput(attrs={'class':'span4'}),
+            'content': Textarea(),
+            }
